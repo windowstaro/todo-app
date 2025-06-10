7bbd1e0ed0a4a308341614b8585e5ab2bb2409fa
narutantan <narumi.030221@gmail.com>
2025-06-10 05:01:18 +0000

    正解

--- src/main/java/com/example/message/MessageApplication.java
+++ src/main/java/com/example/message/MessageApplication.java
@@ -1,4 +1,4 @@
-package com.example.message.controller;
+package com.example.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
