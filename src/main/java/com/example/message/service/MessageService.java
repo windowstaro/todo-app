46435f38faa9cda14bf747a52633d1317d8fb7c5
narutantan <narumi.030221@gmail.com>
2025-06-11 05:41:33 +0000

    edit完了

--- src/main/java/com/example/message/service/MessageService.java
+++ src/main/java/com/example/message/service/MessageService.java
@@ -2,7 +2,12 @@ package com.example.message.service;
 
 import com.example.message.model.Message;
 import com.example.message.repository.MessageRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -22,7 +27,7 @@ public class MessageService {
         repository.save(new Message(name,text));
     }
 
-  public void editMessage(String name, String text, Integer id) {
+    public void editMessage(String name, String text, Integer id) {
         Message message = repository.findById(id).orElseThrow(() -> new RuntimeException("Message not found"));
         message.setName(name);
         message.setText(text);
@@ -32,4 +37,10 @@ public class MessageService {
     public void deleteMessage(Integer id) {
         repository.deleteById(id);
     }
+
+public Message getMessageById(Integer id) {
+    return repository.findById(id).orElseThrow(() -> new RuntimeException("Message not found"));
+}
+
+
 }
